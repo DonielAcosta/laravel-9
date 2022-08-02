9813cad5cfa20c645a8c9716e2cd3a0ed06d2898
Doniel Acosta <donielacosta1995@gmail.com>
2022-08-02 10:53:13 -0400

    controller organizado en rutas

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+/**
+Route::get('/',[PageController::class, 'home'])->name('home');
 
-Route::get('blog', function () {
-  //consultar base de datos
-  $posts = [
-    ['id'=> 1, 'title' => 'PHP', 'slug' => 'php'],
-    ['id'=> 2, 'title' => 'Laravel', 'slug' => 'laravel']
-  ];
-  return view('blog', ['posts' => $posts]) ;
-})->name('blog');
+Route::get('blog', [PageController::class, 'blog'])->name('blog');
 
-Route::get('blog/{slug}', function ($slug) {
-  //consultar base de datos
-  $post = $slug;
-  
-  return view('post', ['post' => $post]) ;
-})->name('post');
+Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
+ */
 
+Route::controller(PageController::class)->group(function(){
+
+  Route::get('/','home')->name('home');
+  Route::get('blog', 'blog')->name('blog');
+  Route::get('blog/{slug}','post')->name('post');
+});
