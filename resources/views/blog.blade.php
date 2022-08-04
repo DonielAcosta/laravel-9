c39515881ec85547eed29b05318eba35466fcfad
Doniel Acosta <donielacosta1995@gmail.com>
2022-08-04 09:59:42 -0400

    modificacion y creacion de vista para post

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('template')
+@extends('template')
 	
 @section('content')
 	
@@ -10,25 +10,11 @@
 	<a href="{{ route('post', $post->slug) }}">
 		{{ $post->title }}
 	</a>
+	<br>
+	<span>{{ $post->user->name }}</span>
 </p>
 @endforeach
 
 {{ $posts->links() }}
 
-@endsection --}}
-
-@extends('templade')
-
-@section('content')
-  <h1>Listado</h1>
-
-  @foreach( $posts as $post )
-  <p>
-    <strong>{{ $post->id }}</strong>
-    <a href="{{ route('post', $post->slug) }}">{{ $post->title }}</a>
-    <br>
-    <span>{{ $post->user->name }}</span>
-  </p>
-  @endforeach
-    {{ $posts->links() }}
 @endsection
