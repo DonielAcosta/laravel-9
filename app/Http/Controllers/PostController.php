e31b7052f39968ed3311c7176f65516a9633413c
Doniel Acosta <donielacosta1995@gmail.com>
2022-08-04 11:40:52 -0400

    vista de editar crear formulario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -16,12 +17,49 @@ class PostController extends Controller
 
   public function create(Post $post) 
   {
+
     return view('posts.create',['post' => $post]);
   }
+
+  public function store(Request $request) 
+  {
+    $request->validate([
+      'title' => 'required|max:255',
+      'slug' => 'required|max:255|unique:posts,slug',
+      'body' => 'required|max:255',
+    ]);
+
+    $post = $request->user()->posts()->create([
+        'title' => $title = $request->title,
+        'slug'  => Str::slug($title),
+        'body'  => $request->body,
+    ]);
+
+    return redirect()->route('posts.edit', $post);
+  }
+
   public function edit(Post $post) 
   {
     return view('posts.edit',['post' => $post]);
   }
+
+  public function update(Request $request, Post $post)
+  {
+    $request->validate([
+      'title' => 'required|max:255',
+      'slug' => 'required|max:255|unique:posts,slug' . $post->id,
+      'body' => 'required|max:255',
+    ]);
+
+    $post->update([
+        'title' => $title = $request->title,
+        'slug'  => Str::slug($title),
+        'body'  => $request->body,
+    ]);
+
+    return redirect()->route('posts.edit', $post);
+  }
+  
   public function destroy(Post $post) 
   {
     $post->delete();
